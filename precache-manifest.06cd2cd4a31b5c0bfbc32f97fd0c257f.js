265623936cc2f70817dfbe6044f9a9d0451aca53
BinaryShrimp <arandombinaryshrimp@gmail.com>
2019-08-12 23:39:43 -0500

    Updates

---
+++ precache-manifest.06cd2cd4a31b5c0bfbc32f97fd0c257f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e222f6340569f2d9b34978ae9c7fbb",
+    "url": "/Monter-Rolodex-Practice/index.html"
+  },
+  {
+    "revision": "527ac063818dd84edff1",
+    "url": "/Monter-Rolodex-Practice/static/css/main.b1d28ea5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monter-Rolodex-Practice/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "527ac063818dd84edff1",
+    "url": "/Monter-Rolodex-Practice/static/js/main.c047beb4.chunk.js"
+  },
+  {
+    "revision": "a5a025f259dd429b3077",
+    "url": "/Monter-Rolodex-Practice/static/js/runtime~main.67ba97f5.js"
+  }
+]);
